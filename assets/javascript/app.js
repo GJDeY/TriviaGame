00dc6e751bff5ec4047656e649187212bcf44669
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-08-11 20:53:31 -0500

    restructured answer arrays, added start button, linked CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    var correct = 0;
+    var incorrect = 0;
+
+
+
+    var qOne = [
+        "What was Elvis' home called?",
+        "Who sings.................",
+        "What year was NSYNC's song 'bye bye bye' released",
+        "What year was 'The Slim Shady LP' released?",
+        "q5",
+        "q"
+    ]
+
+    var answers = [
+        "graceland",
+        "a2",
+        "2000",
+        "1999",
+        "a5",
+        "a"
+    ]
+
+
+    var qOneAns = ["a1", "a2", "a3", "a4"]
+
+    var qTwoAns = ["a5", "a6", "a7", "a8"]
+
+    var qThreeAns = ["a9", "a10", "a11", "a12"]
+
+    var qFourAns = ["a13", "a14", "a15", "a16"]
+
+    var qFiveAns = ["a17", "a18", "a19", "a20"]
+
+
+
+
+
+})
+
+
+
+
