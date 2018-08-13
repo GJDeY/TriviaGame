7f48e2841c8c60e84c04e4c9f99b9d5e99996cf2
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-08-12 19:16:32 -0500

    reconfigured questions, buttons, and CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,78 @@
+//this code gets my page jQuery ready
 $(document).ready(function () {
-    var correct = 0;
-    var incorrect = 0;
+    function game(params) {
 
+    }
+    //question array with answer choices
+    var questions = [
+        {
+            questionOne: "What was Elvis' home called?",//question1 text to display
+            answersOne: ["Graceland", "Iceland", "Poland", "Promisedland"],//answer array within question array
+        },
 
+        {
+            questionTwo: "What year was NSYNC's song 'bye bye bye' released",
+            answersTwo: ["1999", "2000", "2001", "2004"],
+        },
 
-    var qOne = [
-        "What was Elvis' home called?",
-        "Who sings.................",
-        "What year was NSYNC's song 'bye bye bye' released",
-        "What year was 'The Slim Shady LP' released?",
-        "q5",
-        "q"
+        {
+            questionThree: "What is the state bird of Illinois",
+            answersThree: ["Finch", "Sparrow", "Cardinal", "Bald Eagle"],
+        },
+
+        {
+            questionFour: "What was the 1992 men's olympic basketball team nicknamed?",
+            answersFour: ["The Incredibles", "The Great Ones", "The Supreme Team", "The Dream Team"]
+        },
+
+        {
+            questionFive: "What year was The Beach Boys' first album released?",
+            answersFive: ["1942", "1952", "1962", "1972"]
+        }
+
     ]
 
-    var answers = [
-        "graceland",
-        "a2",
-        "2000",
-        "1999",
-        "a5",
-        "a"
-    ]
+    correct = 0;//to log & display correct answers, starting at zero
+    incorrect = 0; //to log & display incorrect answers, starting at zero
+    // if questionOne ===
 
 
-    var qOneAns = ["a1", "a2", "a3", "a4"]
+    //var qOne = [
+    //    "Who sings.................",
+    //    "What year was NSYNC's song 'bye bye bye' released",
+    //    "What year was 'The Slim Shady LP' released?",
+    //    "q5",
+    //    "q"
+    //  ]
 
-    var qTwoAns = ["a5", "a6", "a7", "a8"]
+    //var answers = [
+    //    "graceland",
+    //    "a2",
+    //    "2000",
+    //    "1999",
+    //    "a5",
+    //        "a"
+    //    ]
 
-    var qThreeAns = ["a9", "a10", "a11", "a12"]
+    //answer arrays for each question
+    //  var qOneAns = ["a1", "a2", "a3", "a4"]
 
-    var qFourAns = ["a13", "a14", "a15", "a16"]
+    //   var qTwoAns = ["a5", "a6", "a7", "a8"]
 
-    var qFiveAns = ["a17", "a18", "a19", "a20"]
+    //    var qThreeAns = ["a9", "a10", "a11", "a12"]
+
+    //   var qFourAns = ["a13", "a14", "a15", "a16"]
+
+    //   var qFiveAns = ["a17", "a18", "a19", "a20"]
+
+
+    //function for time remaining, 60000 milliseconds=1 min
+    setTimeout(() => {
+        //if else
+    }, 60000);
+
+
+
 
 
 
